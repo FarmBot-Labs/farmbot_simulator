adaa41683edd30256184aa245534d86a226c9609
Connor Rigby <connor@farmbot.io>
2017-03-26 13:31:18 -0700

    Using https://github.com/BennyQBD/ModernOpenGLTutorial as a base for teh opengl stuffs. draws monkeys.

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule FarmbotSimulator.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     compilers: [:elixir_make] ++ Mix.compilers,
      deps: deps()]
   end
 
@@ -18,16 +19,9 @@ defmodule FarmbotSimulator.Mixfile do
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:elixir_make, "~> 0.4", runtime: false}
+    ]
   end
 end
